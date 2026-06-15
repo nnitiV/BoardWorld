7cfff5671580133daa38526681353af24486e22e
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-06-15 20:10:43 -0300

    refactor(product): refactored product service file.

--- api/src/services/productService.ts
+++ api/src/services/productService.ts
@@ -3,7 +3,11 @@ import { UpdateProduct } from "../types/product.types.js";
 import { AppError } from "../utils/AppError.js";
 
 export const getProductById = async (id: string) => {
-  return await productRepository.getProductById(id);
+  const product = await productRepository.getProductById(id);
+  if (!product) {
+    throw new AppError(`Couldn't find product with id ${id}`, 404);
+  }
+  return product;
 };
 
 export const createNewProduct = async (productData: {
@@ -11,23 +15,40 @@ export const createNewProduct = async (productData: {
   price: string;
   imageUrl: string;
 }) => {
-  const parsedPrice = parseFloat(productData.price);
   const payload = {
     name: productData.name,
-    price: parsedPrice,
+    price: productData.price,
     imageUrl: productData.imageUrl,
   };
   return await productRepository.createProduct(payload);
 };
 
 export const updateProduct = async (id: string, productData: UpdateProduct) => {
+  const product = await getProductById(id);
+  if (!product) {
+    throw new AppError(`Couldn't find product with id ${id}`, 404);
+  }
   return await productRepository.updateProduct(id, productData);
 };
 
 export const restoreProduct = async (id: string) => {
+  const product = await getProductById(id);
+  if (!product) {
+    throw new AppError(`Couldn't find product with id ${id}`, 404);
+  }
+  if (product.isActive) {
+    return product;
+  }
   return await productRepository.restoreProduct(id);
-}
+};
 
 export const deactivateProduct = async (id: string) => {
+  const product = await getProductById(id);
+  if (!product) {
+    throw new AppError(`Couldn't find product with id ${id}`, 404);
+  }
+  if (!product.isActive) {
+    return product;
+  }
   return await productRepository.deactivateProduct(id);
 };
