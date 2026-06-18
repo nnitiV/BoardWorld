d8add12cb213310de782f52b20caf15d2cdf866d
Vitor Augusto Elias Cunha <100286187+nnitiV@users.noreply.github.com>
2026-06-18 08:35:36 -0300

    feat(navbar): Responsive navbar (#6)

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-blue-50">{children}</body>
     </html>
   );
 }
