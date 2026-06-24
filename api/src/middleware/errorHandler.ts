a8db0763c573bb6d31d3b1a3c3511dd290d22ac7
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-06-23 22:52:39 -0300

    feat(asyncHandler): Added async handler in the backend

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -1,19 +1,15 @@
-import { NextFunction, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Response } from "express";
 import { AppError } from "../utils/AppError.js";
 import { ZodError } from "zod";
 
-export default function globalErrorHandler(
-  err: any,
-  res: Response,
-  next: NextFunction,
-) {
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let status = 500;
   let message: any = "Internal server error";
 
   if (err instanceof AppError) {
     status = err.status;
     message = err.message;
-  }else if (err instanceof ZodError) {
+  } else if (err instanceof ZodError) {
     status = 400;
     
     message = err.issues.map(issue => ({
@@ -26,4 +22,6 @@ export default function globalErrorHandler(
   }
 
   res.status(status).json({ message });
-}
+};
+
+export default globalErrorHandler;
